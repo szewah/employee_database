60562fd92bceb363524eeea1906efdeccaf5be50
Szewah <swgrleong@gmail.com>
2019-02-27 14:12:29 -0500

    Updated total billed

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
+//Firebase key
 
-
+//Initialize the key
 firebase.initializeApp(config);
+//Assign it to a variable database
 var database = firebase.database();
 
+//Initialize variables for the database
 var formName;
 var formRole;
 var formStartDate;
 var formSalary;
 
+//Click event that grabs the input value and submits it to the Firebase database
 $("#add-employee").on('click', function() {
     event.preventDefault();
     formName = $("#name").val().trim();
     formRole = $("#role").val();
     formStartDate = $("#start-date").val();
     formSalary = $("#monthly-salary").val();
-
+    //Pushing the values into an object. The push() method 
+    //creates a unique id when the data is pushed.
     database.ref().push({
         formName: formName,
         formRole: formRole,
         formStartDate: formStartDate,
         formSalary: formSalary,
       }); 
-      //emptying the input on the front-end
+      //Empty the inputs on submit
       $("#name").val("");
       $("#role").val("");
       $("#start-date").val("");
       $("#monthly-salary").val("");
 });
 
+//Create a reference to the object
+// Child_added event is used when retrieving a list of items from the database. 
+// Unlike value which returns the entire contents of the location, child_added is 
+// triggered once for each existing child and then again every time a new child 
+// is added to the specified path. The event callback is passed a snapshot 
+// containing the new child's data.
 database.ref().on('child_added', function(snapshot) {
   renderRow(snapshot.val()); 
   },     
@@ -37,7 +48,9 @@ database.ref().on('child_added', function(snapshot) {
   });
   
 function renderRow(obj) {
-    var salaryInfo = calculateSalary(obj.formStartDate);
+  //This draws the information from the database rather than from the value 
+  //input in the DOM;
+    var salaryInfo = calculateSalary(obj.formStartDate, obj.formSalary);
     var tableRow = $("<tr class='table-row'>");
     var tableDataName = $("<td class='employee-name'>").text(obj.formName);
     var tableDataRole = $("<td class='role'>").text(obj.formRole);
@@ -45,25 +58,24 @@ function renderRow(obj) {
     var tableSalary = $("<td class='salary'>").text(obj.formSalary);
     var tableMonthsWorked= $("<td class='months-worked'>").text(salaryInfo[0]);
     var tableTotalBilled= $("<td class='total-billed'>").text(salaryInfo[1]);
-    tableRow.append(tableDataName, tableDataRole, tableStartDate, tableSalary, tableMonthsWorked, tableTotalBilled);
+    tableRow.append(tableDataName, tableDataRole, tableStartDate, tableSalary, 
+    tableMonthsWorked, tableTotalBilled);
     $(".table").append(tableRow);
 };
 
-function calculateSalary(date) {
+//This function calculates the salary from months worked and salary paid each month
+//It uses moment.js javascript library to organize time and dates
+function calculateSalary(date, salary) {
     var startDate = date;
-    console.log(date);
     var dateFormat = "MM-DD-YYYY";
     var convertedDate = moment(startDate, dateFormat);
     var workingDuration = convertedDate.diff(moment(), "months");
     var positiveDuration = Math.abs(workingDuration);
-    console.log(positiveDuration);
-    var formSalary = $("#monthly-salary").val();
+    var formSalary = salary;
     var totalSalary = positiveDuration * formSalary;
-    console.log(totalSalary);
     return [positiveDuration, totalSalary]
 };
 
-// 1. how to push the date difference into the table
 
 
 
