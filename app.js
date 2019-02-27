04531f1e0e26c664689219997b57838c84d2c423
Szewah <swgrleong@gmail.com>
2019-02-26 19:05:46 -0500

    Calculating totalbilled

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var config = {
+  apiKey: "AIzaSyA2vZH3EegVXUPAw3AOknEGYOu8auYv3uE",
+  authDomain: "fabulous-project-demo.firebaseapp.com",
+  databaseURL: "https://fabulous-project-demo.firebaseio.com",
+  projectId: "fabulous-project-demo",
+  storageBucket: "fabulous-project-demo.appspot.com",
+  messagingSenderId: "322665992078"
+};
 
-  };
 
 firebase.initializeApp(config);
 var database = firebase.database();
@@ -9,13 +15,10 @@ var formName;
 var formRole;
 var formStartDate;
 var formSalary;
-var monthsWorked;
-var totalBill;
 
 $("#add-employee").on('click', function() {
     event.preventDefault();
-    console.log("I was clicked");
-    formName = $("#name").val();
+    formName = $("#name").val().trim();
     formRole = $("#role").val();
     formStartDate = $("#start-date").val();
     formSalary = $("#monthly-salary").val();
@@ -25,14 +28,16 @@ $("#add-employee").on('click', function() {
         formRole: formRole,
         formStartDate: formStartDate,
         formSalary: formSalary,
-        // formMonthsWorked: monthsWorked,
-        // formTotalBilled: totalBill
       }); 
-      calculateSalary();
+      //emptying the input on the front-end
+      $("#name").val("");
+      $("#role").val("");
+      $("#start-date").val("");
+      $("#monthly-salary").val("");
 });
 
 database.ref().on('child_added', function(snapshot) {
-    renderRow(snapshot.val()); 
+  renderRow(snapshot.val()); 
   },     
   // Create Error Handling
   function(errorObject) {
@@ -40,23 +45,21 @@ database.ref().on('child_added', function(snapshot) {
   });
   
 function renderRow(obj) {
+    var salaryInfo = calculateSalary(obj.formStartDate);
     var tableRow = $("<tr class='table-row'>");
     var tableDataName = $("<td class='employee-name'>").text(obj.formName);
     var tableDataRole = $("<td class='role'>").text(obj.formRole);
     var tableStartDate = $("<td class='start-date'>").text(obj.formStartDate);
     var tableSalary = $("<td class='salary'>").text(obj.formSalary);
-    // var tableMonthsWorked= $("<td class='months-worked'>").text(obj.formMonthsWorked);
-    // var tableTotalBilled= $("<td class='total-billed'>").text(obj.formTotalBilled);
-    tableRow.append(tableDataName, tableDataRole, tableStartDate, tableSalary);
+    var tableMonthsWorked= $("<td class='months-worked'>").text(salaryInfo[0]);
+    var tableTotalBilled= $("<td class='total-billed'>").text(salaryInfo[1]);
+    tableRow.append(tableDataName, tableDataRole, tableStartDate, tableSalary, tableMonthsWorked, tableTotalBilled);
     $(".table").append(tableRow);
 };
 
-// function dateConversion() {
-  
-// }
-
-function calculateSalary() {
-    var startDate = $("#start-date").val();
+function calculateSalary(date) {
+    var startDate = date;
+    console.log(date);
     var dateFormat = "MM-DD-YYYY";
     var convertedDate = moment(startDate, dateFormat);
     var workingDuration = convertedDate.diff(moment(), "months");
@@ -65,6 +68,7 @@ function calculateSalary() {
     var formSalary = $("#monthly-salary").val();
     var totalSalary = positiveDuration * formSalary;
     console.log(totalSalary);
+    return [positiveDuration, totalSalary]
 };
 
 // 1. how to push the date difference into the table
